fb09ded7d39c0eb6a15cba0b575ef034bc0e3c2e
unknown <mmthakor@gmail.com>
2021-10-02 13:18:57 +0530

    first commit

---
+++ scripts/scriptIplResults.js
@@ -0,0 +1,10 @@
+//firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDIhsjjWPtNy6-2vDgQCEh6slMBIMqtfJw",
+    authDomain: "aca-25--sports-website-1d22e.firebaseapp.com",
+    databaseURL: "https://aca-25--sports-website-1d22e-default-rtdb.firebaseio.com",
+    projectId: "aca-25--sports-website-1d22e",
+    storageBucket: "aca-25--sports-website-1d22e.appspot.com",
+    messagingSenderId: "774146852579",
+    appId: "1:774146852579:web:8d632965d5675b25e8a7d6"
+  };
